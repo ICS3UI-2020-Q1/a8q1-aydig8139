34ce54c1f14415ef3a7425eaf1ff888f363b1655
aydig8139 <aydig8139@wrdsb.ca>
2020-10-29 15:42:29 +0000

    finished question to say hello+name

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+
+  JTextField nameInput;
+
+  JButton helloButton;
+
+  JLabel nameOutput;
 
 
   // Method to assemble our GUI
@@ -18,7 +24,48 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
- 
+
+    //initialize the main JPanel
+    mainPanel = new JPanel();
+
+    //disable any layout helpers
+    mainPanel.setLayout(null);
+
+    //initialize the input text fields
+    nameInput = new JTextField();
+
+    //set the location and size 
+    nameInput.setBounds(30, 20, 100, 20);
+
+    //initialize the button
+    helloButton = new JButton("Say Hello");
+
+    //set the size and location of the buttons
+    helloButton.setBounds(150, 100, 100, 20);
+
+    //add an action listener to the button
+    helloButton.addActionListener(this);
+
+    //set the action command on the button
+    helloButton.setActionCommand("Say Hello");
+
+    //create label
+    nameOutput = new JLabel();
+
+    //layout the label by setting the coordinate and size 
+    nameOutput.setBounds(30,60,100,20);
+
+    //add the label to the main panel 
+    mainPanel.add(nameOutput);
+
+    //add the button to the panel
+    mainPanel.add(helloButton);
+
+    //add the text field to the panel
+    mainPanel.add(nameInput);
+
+    //add the panel to the window
+    frame.add(mainPanel);
     
 
   }
@@ -28,8 +75,13 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
-  }
+    if(command.equals("Say Hello")){
+      //get the text from the text box
+      String nameText = nameInput.getText();
+      nameOutput.setText("Hello " + nameText);
 
+  }
+  }
   // Main method to start our program
   public static void main(String[] args){
     // Creates an instance of our program
